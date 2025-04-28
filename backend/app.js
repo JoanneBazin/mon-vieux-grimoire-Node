126f73c2325bee0bc12c26de38a33fdf438f9a0e
Joanne <joanne.bazin@gmail.com>
2025-04-28 14:10:01 +0200

    chore: setup backend server Express, CORS config and project structure init

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use((req, res) => {
+  res.status(200).json({
+    message: "Init backend",
+  });
+});
+
+module.exports = app;
