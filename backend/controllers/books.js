8929642f3b6e3ea4ab5fcc358a1e38eda7cd09fd
Joanne <joanne.bazin@gmail.com>
2025-04-28 16:16:45 +0200

    feat(books): add GET books route and controller

---
+++ backend/controllers/books.js
@@ -0,0 +1,23 @@
+const Book = require("../models/Book");
+
+exports.createBook = (req, res, next) => {
+  const newBook = new Book({
+    ...req.body,
+  })
+    .then((books) => {
+      res.status(200).json(books);
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
+};
+
+exports.getAllBooks = (req, res, next) => {
+  Book.find()
+    .then((books) => {
+      res.status(200).json(books);
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
+};
