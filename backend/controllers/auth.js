b515c37e7b67ed92b25b813169e9141f58cfef7f
Joanne <joanne.bazin@gmail.com>
2025-04-29 15:23:37 +0200

    feat(auth): add login route & controller

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 exports.signup = (req, res, next) => {
   bcrypt.hash(req.body.password, 10).then((hash) => {
@@ -12,4 +14,34 @@ exports.signup = (req, res, next) => {
       .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
       .catch((error) => res.status(400).json({ error }));
   });
+};
+
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error: "Identifiants incorrects." });
+      } else {
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then((valid) => {
+            if (!valid) {
+              return res
+                .status(401)
+                .json({ error: "Identifiants incorrects." });
+            } else {
+              res.status(200).json({
+                userId: user._id,
+                token: jwt.sign(
+                  { userId: user._id },
+                  process.env.TOKEN_SECRET,
+                  { expiresIn: "24h" }
+                ),
+              });
+            }
+          })
+          .catch((error) => res.status(500).json({ error }));
+      }
+    })
+    .catch((error) => res.status(500).json({ error }));
 };
