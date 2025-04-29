729c2af326898d3a84247d576aa96ae10e1417bf
Joanne <joanne.bazin@gmail.com>
2025-04-29 17:51:12 +0200

    feat(books): add delete route & controller

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -6,10 +6,11 @@ const multer = require("../middleware/multer-config");
 
 const booksCtrl = require("../controllers/books");
 
-router.get("/", booksCtrl.getAllBooks);
+router.get("/", auth, booksCtrl.getAllBooks);
 router.post("/", auth, multer, booksCtrl.addBook);
-router.get("/bestrating", booksCtrl.getBestRatingBooks);
-router.get("/:id", booksCtrl.getOneBook);
+router.get("/bestrating", auth, booksCtrl.getBestRatingBooks);
+router.get("/:id", auth, booksCtrl.getOneBook);
 router.put("/:id", auth, multer, booksCtrl.updateBook);
+router.delete("/:id", auth, multer, booksCtrl.deleteBook);
 
 module.exports = router;
